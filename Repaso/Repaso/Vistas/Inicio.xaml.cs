f2614ae420a46c8508636681022b26b7eb013f29
Fausto Almeida <faustecn@hotmail.com>
2023-01-10 11:32:36 -0500

    Agregar archivos de proyecto.

---
+++ Repaso/Repaso/Vistas/Inicio.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Repaso.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Inicio : ContentPage
+    {
+        public Inicio()
+        {
+            InitializeComponent();
+        }
+
+        private void btnIniciar_Clicked(object sender, EventArgs e)
+        {
+            
+            Navigation.PushAsync(new registro(txtUsuario.Text));
+        }
+    }
+}
---
+++ Repaso/Repaso/Vistas/registro.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Repaso.Vistas
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class registro : ContentPage
+    {
+        public registro( string usuario)
+        {
+            InitializeComponent();
+            lblUsuario.Text = "Bienvenido " + usuario;
+        }
+
+        private void bntCalcular_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                double precio = Convert.ToDouble(txtPrecio.Text);
+                int cantidad = Convert.ToInt32(txtCantidad.Text);
+                double valor = precio * cantidad;
+
+                DisplayAlert("Valor total del producto", valor.ToString(), "Cerrar");
+            }
+            catch(Exception ex) {
+
+                DisplayAlert("Alerta", ex.Message, "Cerrar");
+            }
+
+        }
+    }
+}
